b4567653c159c6258a2544b7a74ced7289b653ed
朴野归踪 <2472906920@qq.com>
2017-10-25 19:07:44 +0800

    add
    add

--- src/component/Input001/index.jsx
+++ src/component/Input001/index.jsx
@@ -8,25 +8,37 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: '',
+      placeholder: this.props.data.placeholder,
+      type:'text',
+      value:''
     };
+
+    
   }
   emitEmpty = () => {
     this.userNameInput.focus();
-    this.setState({ userName: '' });
+    this.setState({ 
+      value: '' 
+    });
   }
   onChangeUserName = (e) => {
-    this.setState({ userName: e.target.value });
+    
+    this.setState({ 
+      value: e.target.value 
+    });
+
+    this.props.data.changePrice(e.target.value)
+
   }
   render() {
-    const { userName } = this.state;
-    const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
+    const { value } = this.state;
+    const suffix = value ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
     return (
       <Input
-        placeholder="Enter your userName"
+        placeholder={this.state.placeholder}
         prefix={<Icon type="user" />}
         suffix={suffix}
-        value={userName}
+        value={value}
         onChange={this.onChangeUserName}
         ref={node => this.userNameInput = node}
       />
--- src/containers/LoginView.jsx
+++ src/containers/LoginView.jsx
@@ -2,21 +2,46 @@ import React, { Component } from 'react';
 
 import { HeadNav, Input001 } from '../component'
 
+import { Button } from 'antd';
+
 
 export default class LoginView extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      price: 0
+    }
+  }
+
+  changePrice(price){
+    this.setState({price: price})
+  }
+
+  handleMenuClick(e) {
+    console.log( this.state );
+  }
+
   render() {
+
+    //  changePrice这个方法是要传到子组件里去的
+    let data = {
+      placeholder: "请输入用户名",
+      changePrice: this.changePrice.bind(this)
+    }
+
+
     return(
       <div className="login-view">
 
         <div className='login-wrap'>
           <div className='login-info'>111111</div>
           <div className='login-container'>
-              <Input001 />
+              <Input001 data={data}/>
               2222222
           </div>
-        
+          <Button onClick={this.handleMenuClick.bind(this)}>00000</Button >
         </div>
-        
         
       </div>
     )
